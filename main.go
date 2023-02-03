eee1361c8535e442373cc3f027d734e312dc56a5
blaiseniyo <blaiseniyonkuru12@gmail.com>
2023-02-03 13:30:59 +0200

    add database connection configurations

--- main.go
+++ main.go
@@ -1,16 +1,33 @@
 package main
 
 import (
-  "net/http"
-  "github.com/gin-gonic/gin"
+	"fmt"
+
+	"github.com/Blaiseniyo/go-simple-bank-backend/db"
+	"github.com/Blaiseniyo/go-simple-bank-backend/models"
 )
 
+// import (
+//   "net/http"
+//   "github.com/gin-gonic/gin"
+// )
+
 func main() {
-  r := gin.Default()
-  r.GET("/ping", func(c *gin.Context) {
-    c.JSON(http.StatusOK, gin.H{
-      "message": "pong",
-    })
-  })
-  r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+  // r := gin.Default()
+  // r.GET("/ping", func(c *gin.Context) {
+  //   c.JSON(http.StatusOK, gin.H{
+  //     "message": "pong",
+  //   })
+  // })
+  // r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+  DB := db.Connect()
+  accounts := []models.Account{} 
+  account := models.Account{
+    Owner: "Blaise niyonkuru",
+    Currency: "FRW",
+    Balance: 1000000,
+  } 
+  DB.Create(&account)
+  DB.Find(&accounts)
+  fmt.Println(accounts)
 }
